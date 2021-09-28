92c6a3cf3adfddac9fe4b319f27fbf5e17cadcc7
Praneeth Gajula <npgajula@gmail.com>
2021-09-28 16:08:10 -0400

    Utils Trial 2

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -9,6 +9,8 @@ __author__ = "730394749"
 def all(a: list[int], b: int) -> bool:
     """Figure out if all of a is the same as b."""
     length: int = len(a)
+    if length == 0:
+        return False
     i: int = 0
     while i < length:
         if a[i] != b:
@@ -22,6 +24,8 @@ def is_equal(a: list[int], b: list[int]) -> bool:
     """Figure out if two lists are equal."""
     length_one: int = len(a)
     length_two: int = len(b)
+    if length_one != length_two:
+        return False
     if length_one < length_two:
         return False
     else:
