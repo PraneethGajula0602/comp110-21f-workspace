cc41c779dac060db8db2123e51637db6785b2ca0
Praneeth Gajula <npgajula@gmail.com>
2021-10-05 20:08:18 -0400

    Trial 6

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,22 +7,25 @@ __author__ = "730394749"
 
 
 def test_only_evens_one() -> None:
-    """Testing only_evens for an empty set"""
+    """Testing only_evens for an empty set."""
     xs: list[int] = []
     assert only_evens(xs) == []
 
 
 def test_only_evens_two() -> None:
+    """Testing only_evens for a use case."""
     xs: list[int] = [1, 2, 4, 5]
     assert only_evens(xs) == [2, 4]
 
 
 def test_only_evens_three() -> None:
+    """Testing only_evens for a set with only odd numbers."""
     xs: list[int] = [1, 3, 5, 7]
     assert only_evens(xs) == []
 
 
 def test_sub_one() -> None:
+    """Testing sub for an empty set."""
     xs: list[int] = []
     y: int = 1
     z: int = 3
@@ -30,6 +33,7 @@ def test_sub_one() -> None:
 
 
 def test_sub_two() -> None:
+    """Testing sub for a use case."""
     xs: list[int] = [1, 2, 3, 4, 5]
     y: int = 2
     z: int = 4
@@ -37,6 +41,7 @@ def test_sub_two() -> None:
 
 
 def test_sub_three() -> None:
+    """Testing sub for the given example."""
     xs: list[int] = [10, 20, 30, 40]
     y: int = 1
     z: int = 3
@@ -44,18 +49,21 @@ def test_sub_three() -> None:
 
 
 def test_concat_one() -> None:
+    """Testing concat for an empty set."""
     xs: list[int] = []
     ys: list[int] = []
     assert concat(xs, ys) == []
 
 
 def test_concat_two() -> None:
+    """Testing concat for a use case."""
     xs: list[int] = [1, 2, 3, 4]
     ys: list[int] = [5, 6, 7, 8]
     assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 def test_concat_three() -> None:
+    """Testing concat for a use case."""
     xs: list[int] = [1, 3, 5, 7]
     ys: list[int] = [2, 4, 6, 8]
     assert concat(xs, ys) == [1, 3, 5, 7, 2, 4, 6, 8]
