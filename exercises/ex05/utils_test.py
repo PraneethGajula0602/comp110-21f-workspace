57a80ca45601cb5cf92d8030058bfcf3cc0e1207
Praneeth Gajula <npgajula@gmail.com>
2021-10-05 20:04:17 -0400

    Trial 4

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,9 +1,61 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "730394749"
 
 
-def test_only_evens()
+def test_only_evens_one() -> None:
+    """Testing only_evens for an empty set"""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_two() -> None:
+    xs: list[int] = [1, 2, 4, 5]
+    assert only_evens(xs) == [2, 4]
+
+
+def test_only_evens_three() -> None:
+    xs: list[int] = [1, 3, 5, 7]
+    assert only_evens(xs) == []
+
+
+def test_sub_one() -> None:
+    xs: list[int] = []
+    y: int = 1
+    z: int = 3
+    assert sub(xs, y, z) == []
+
+
+def test_sub_two() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5]
+    y: int = 2
+    z: int = 4
+    assert sub(xs, y, z) == [3, 4]
+
+
+def test_sub_three() -> None:
+    xs: list[int] = [10, 20, 30, 40]
+    y: int = 1
+    z: int = 3
+    assert sub(xs, y, z) == [20, 30]
+
+
+def test_concat_one() -> None:
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_two() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = [5, 6, 7, 8]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_three() -> None:
+    xs: list[int] = [1, 3, 5, 7]
+    ys: list[int] = [2, 4, 6, 8]
+    assert concat(xs, ys) == [1, 3, 5, 7, 2, 4, 6, 8]
