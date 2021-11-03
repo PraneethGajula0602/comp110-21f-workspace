d0772cf950e83e0923d14819369f543fe4e6cd96
Praneeth Gajula <npgajula@gmail.com>
2021-11-02 22:47:50 -0400

    Trial 1

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,73 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730394749"
 
 # Define your functions below
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a CSV into a table."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oritented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(col_table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    result: dict[str, list[str]] = {}
+    for key in col_table:
+        result_list: list[str] = []
+        i: int = 0
+        while i < n:
+            result_list.append(col_table[key][i])
+            i += 1
+        result[key] = result_list
+    return result
+
+
+def select(col_table: dict[str, list[str]], name: list[str]) -> dict[str, list[str]]:
+    result: dict[str, list[str]] = {}
+    i: int = 0
+    while i < len(name):
+        print(col_table[name[i]])
+        result[name[i]] = col_table[name[i]]
+        i += 1
+    return result
+
+
+def concat(col_table_one: dict[str, list[str]], col_table_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    result: dict[str, list[str]] = {}
+    for key_one in col_table_one:
+        result[key_one] = col_table_one[key_one]
+    for key_two in col_table_two:
+        if key_two in result:
+            i: int = 0
+            while i < len(col_table_two[key_two]):
+                result[key_two].append(col_table_two[key_two][i])
+                i += 1
+        else:
+            result[key_two] = col_table_two[key_two]
+    return result
