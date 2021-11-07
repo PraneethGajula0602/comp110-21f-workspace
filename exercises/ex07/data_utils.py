45a8dc9f3e511b9c58f419ff0c5412c2a43cb738
Praneeth Gajula <npgajula@gmail.com>
2021-11-06 20:15:32 -0400

    Extra

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -37,18 +37,26 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
 
 
 def head(col_table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Shows you the first n rows of your table."""
     result: dict[str, list[str]] = {}
-    for key in col_table:
-        result_list: list[str] = []
-        i: int = 0
-        while i < n:
-            result_list.append(col_table[key][i])
-            i += 1
-        result[key] = result_list
+    for keys in col_table:
+        count_list: list[str] = []
+        count_list.append(keys)
+    if len(count_list) + 1 <= n:
+        return col_table
+    else:
+        for key in col_table:
+            result_list: list[str] = []
+            i: int = 0
+            while i < n:
+                result_list.append(col_table[key][i])
+                i += 1
+            result[key] = result_list
     return result
 
 
 def select(col_table: dict[str, list[str]], name: list[str]) -> dict[str, list[str]]:
+    """Selects only the rows you want based on the column names you input."""
     result: dict[str, list[str]] = {}
     i: int = 0
     while i < len(name):
@@ -59,6 +67,7 @@ def select(col_table: dict[str, list[str]], name: list[str]) -> dict[str, list[s
 
 
 def concat(col_table_one: dict[str, list[str]], col_table_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Puts together two groups of data into one."""
     result: dict[str, list[str]] = {}
     for key_one in col_table_one:
         result[key_one] = col_table_one[key_one]
